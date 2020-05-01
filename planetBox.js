7bd766be35c37f809ea57f9fab2bced4777c6536
JoakimJohesan <joakim-a@hotmail.com>
2020-05-01 13:34:23 +0200

    Fixed styling and behaviour when clicking a planet name.

--- planetBox.js
+++ planetBox.js
@@ -6,53 +6,68 @@ class planetBox {
 
     constructor() {
         this.planets = [];
-        this.numberOfLoadedPlanets = this.planets.length;
         this.addPlanetsToDocument();
-        this.loadMoreButton()
+        //this.loadMoreButton()
     }
 
     async fetchPlanets() {
-        for (let i = this.numberOfLoadedPlanets + 1; i <= this.numberOfLoadedPlanets + 10; i++) {
+        let numberOfPlanets = this.planets.length;
+        for (let i = (numberOfPlanets + 1); i <= numberOfPlanets + 10; i++) {
             let planet = await fetchPlanet(i);
             if (planet !== false) {
                 this.planets[i] = planet;
             }
         }
-        this.numberOfLoadedPlanets = this.planets.length;
     }
 
     async addPlanetsToDocument() {
         await this.fetchPlanets();
         this.planets.forEach(planet => {
+
             let divTag = document.createElement('div');
             divTag.classList.add("planet");
+
+            let headerTag = document.createElement('h2');
+            headerTag.textContent = planet.name;
+            headerTag.id = planet.name;
+
             let pTag = document.createElement('p');
-            pTag.textContent = "Name: " + planet.name + " Population: " + planet.population;
+            pTag.textContent = "Population: " + planet.population;
+            pTag.id = "p" + planet.name;
+
             let imgTag = document.createElement('img');
             imgTag.src = planet.imgUrl;
-            pTag.id = planet.name;
+
             divTag.appendChild(imgTag);
+            divTag.appendChild(headerTag);
             divTag.appendChild(pTag);
             document.body.appendChild(divTag);
 
-            pTag.addEventListener('click', (event) => {
-                let tag = event.target;
-
-                let selectedPlanet;
-                this.planets.forEach(planet => {
-                    if (planet.name === tag.id) {
-                        selectedPlanet = planet;
-                    }
-                });
-                tag.textContent =
-                    "Name: " + selectedPlanet.name
-                    + " Population: " + selectedPlanet.population
-                    + " Climate: " + selectedPlanet.climate
-                    + " Terrain: " + selectedPlanet.terrain
-                    + " Diameter: " + selectedPlanet.diameter;
-            });
+            headerTag.addEventListener('click', (event) => {
+                this.addAdditionalDetailsToCard(event)
+            }, false);
         })
-    }
+    };
+
+
+    addAdditionalDetailsToCard(event) {
+        let tag = event.target;
+
+        let selectedPlanet;
+        this.planets.forEach(planet => {
+            if (planet.name === tag.id) {
+                selectedPlanet = planet;
+            }
+        });
+
+        document.getElementById("p" + selectedPlanet.name).textContent =
+            " Population: " + selectedPlanet.population
+            + " Climate: " + selectedPlanet.climate
+            + " Terrain: " + selectedPlanet.terrain
+            + " Diameter: " + selectedPlanet.diameter;
+
+    };
+
 
     loadMoreButton() {
         let loadMoreButton = document.createElement('button');
@@ -66,10 +81,14 @@ class planetBox {
 
 const init = () => {
     let pb = new planetBox();
-    document.getElementById("loadMoreButton")
+
+    //This does not work properly yet.
+    /*document.getElementById("loadMoreButton")
         .addEventListener('click', () => {
             pb.addPlanetsToDocument();
         }, false);
+
+     */
 };
 
 window.addEventListener('DOMContentLoaded', init, false);
