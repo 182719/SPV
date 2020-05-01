96799afa9166cc6bde368b02d883644d2e3c6188
JoakimJohesan <joakim-a@hotmail.com>
2020-05-01 13:06:45 +0200

    Oppgave 1 og 2 prototype ferdig.

--- planetBox.js
+++ planetBox.js
@@ -1,39 +1,75 @@
+"use strict";
+
 import {fetchPlanet} from "./planetFetch.js";
 
 class planetBox {
 
     constructor() {
         this.planets = [];
-        for (let i = 0; i < 11; i++) {
-            let planet = fetchPlanet(i);
+        this.numberOfLoadedPlanets = this.planets.length;
+        this.addPlanetsToDocument();
+        this.loadMoreButton()
+    }
+
+    async fetchPlanets() {
+        for (let i = this.numberOfLoadedPlanets + 1; i <= this.numberOfLoadedPlanets + 10; i++) {
+            let planet = await fetchPlanet(i);
             if (planet !== false) {
                 this.planets[i] = planet;
             }
         }
-        this.writePlanets();
-
+        this.numberOfLoadedPlanets = this.planets.length;
     }
 
-    writePlanets() {
-        for (const planet in this.planets) {
-            let div = document.createElement('div');
-            div.classList.add("planet");
+    async addPlanetsToDocument() {
+        await this.fetchPlanets();
+        this.planets.forEach(planet => {
+            let divTag = document.createElement('div');
+            divTag.classList.add("planet");
             let pTag = document.createElement('p');
-            pTag.textContent = "Name: " + planet.name + "/n Population: " + planet.population;
-            let img = document.createElement('img');
-            img.src = planet.imgUrl;
+            pTag.textContent = "Name: " + planet.name + " Population: " + planet.population;
+            let imgTag = document.createElement('img');
+            imgTag.src = planet.imgUrl;
+            pTag.id = planet.name;
+            divTag.appendChild(imgTag);
+            divTag.appendChild(pTag);
+            document.body.appendChild(divTag);
 
-            pTag.appendChild(img);
-            div.appendChild(pTag);
+            pTag.addEventListener('click', (event) => {
+                let tag = event.target;
 
-        }
+                let selectedPlanet;
+                this.planets.forEach(planet => {
+                    if (planet.name === tag.id) {
+                        selectedPlanet = planet;
+                    }
+                });
+                tag.textContent =
+                    "Name: " + selectedPlanet.name
+                    + " Population: " + selectedPlanet.population
+                    + " Climate: " + selectedPlanet.climate
+                    + " Terrain: " + selectedPlanet.terrain
+                    + " Diameter: " + selectedPlanet.diameter;
+            });
+        })
+    }
 
+    loadMoreButton() {
+        let loadMoreButton = document.createElement('button');
+        loadMoreButton.textContent = "Load more planets";
+        loadMoreButton.type = "Button";
+        loadMoreButton.id = "loadMoreButton";
 
+        document.body.appendChild(loadMoreButton);
     }
 }
 
 const init = () => {
-    new planetBox();
+    let pb = new planetBox();
+    document.getElementById("loadMoreButton")
+        .addEventListener('click', () => {
+            pb.addPlanetsToDocument();
+        }, false);
 };
 
 window.addEventListener('DOMContentLoaded', init, false);
--- planetFetch.js
+++ planetFetch.js
@@ -1,7 +1,7 @@
 export {fetchPlanet};
 const fetchPlanet = async (id) => {
-    let fetchUrl = "https://spv-swapi.herokuapp.com//api/planets/" + id + "/";
-    let imgUrl = "https://starwars-visualguide.com/assets/img/planets/" + id + ".jpg";
+    let fetchUrl = "https://spv-swapi.herokuapp.com/api/planets/" + id + "/";
+    let imgUrl = "https://starwars-visualguide.com/assets/img/planets/" + (id + 1) + ".jpg";
     let returnPlanet = {};
 
     await fetch(fetchUrl)
@@ -9,10 +9,13 @@ const fetchPlanet = async (id) => {
             return resolve.json();
         })
         .then(data => {
-            if (data.detail != null) {
+            if (data.detail !== "Not found.") {
                 returnPlanet.name = data.name;
                 returnPlanet.population = data.population;
                 returnPlanet.imgUrl = imgUrl;
+                returnPlanet.climate = data.climate;
+                returnPlanet.terrain = data.terrain;
+                returnPlanet.diameter = data.diameter;
             } else {
                 return false;
             }
